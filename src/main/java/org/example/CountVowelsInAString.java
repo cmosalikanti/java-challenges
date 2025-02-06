c28d8afee6027bb7a00e578449fc3c4875793086
Chandra Sekhar Mosalikanti <mcsekhar777@gmail.com>
2025-02-06 11:21:52 +0000

    added a challenge for counting vowels, finding largest and smallest.

---
+++ src/main/java/org/example/CountVowelsInAString.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CountVowelsInAString {
+    public static void main(String[] args) {
+        countVowelsIn("soudsdsdsa");
+    }
+
+    private static void countVowelsIn(String string) {
+        List<Character> vowels = new ArrayList<>(Arrays.asList('a', 'e','i','o', 'u'));
+
+        int vowelCount = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (vowels.contains(string.charAt(i))) {
+                vowelCount++;
+            }
+        }
+
+        System.out.println("Vowel count: " + vowelCount);
+    }
+}
---
+++ src/main/java/org/example/LargestAndSmallest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class LargestAndSmallest {
+    public static void main(String[] args) {
+        findLargestAndSmallest(Arrays.asList(1,2,3,4,5));
+    }
+
+    private static void findLargestAndSmallest(List<Integer> numbers) {
+        Collections.sort(numbers);
+        System.out.println("Smallest: " + numbers.get(0));
+        System.out.println("Largest: " + numbers.get(numbers.size() - 1));
+    }
+}
